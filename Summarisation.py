ce95e371857da57eaeeb4c8c26fc5e24ad0304cb
JackIHill <jackhill1414@gmail.com>
2025-02-13 16:51:33 +0000

    add summarisation between a date range

--- Summarisation.py
+++ Summarisation.py
@@ -6,35 +6,37 @@ from sqlalchemy.engine.base import Connection
 
 from credentials.SQL_Credentials import username, password, server, database, driver
 
-from openai import OpenAI
 from credentials.OpenAI_API_Key import API_KEY
 
 import tools.summarytools as summtools
 import tools.aitools as aitools
 
-client, engine = aitools.establish_connection(API_KEY, username, password, server, database, driver)
+import os
 
-YEAR_MONTH: str = 'SEP-24'
+DEFAULT_NUM_WORKERS = os.cpu_count() * 5
+client, engine = aitools.establish_connection(API_KEY, username, password, server, database, driver, DEFAULT_NUM_WORKERS)
 
-MMM_YYtoDate = f"""
-            SELECT ActualDate FROM Dates WHERE ActualDate = CONVERT(DATE, CONCAT('01-', '{YEAR_MONTH}'))
-            """
 
-def process_summaries(obj, conn: Connection, date_int: int, date_string: str):
+def process_summaries(obj, conn: Connection, rep_MMM_YY: str, num_months: int):
+
+    start_date_id, end_date_id = aitools.start_end_date(rep_MMM_YY, num_months)
+    conn.execute(sa.text(summtools.date_range_insert(start_date_id, end_date_id, num_months)))
+    
     completed = 0
     failed = 0
     offset = 0
     try_count = 1    
-    while True:
-        
-        remaining = obj.get_count_remaining(conn, date_int, date_string) - failed
+
+    while (obj.table_name == 'Venue' and completed <= 5) or (obj.table_name != 'Venue'):
+        remaining = obj.get_count_remaining(conn, start_date_id, end_date_id) - failed
       
-        if remaining == 0 : 
-            print(f'All {obj.table_name}s Summarised For {YEAR_MONTH.title()}.')
+        if remaining == 0: 
+            print(f'All {obj.table_name}s Summarised For {rep_MMM_YY.title()}.')
             break
         
         # Grab all non-null ReviewText reviews for a venue. 
-        input_tbl = obj.get_remaining_rows(conn, offset, date_int, date_string)
+        input_tbl = obj.get_remaining_rows(conn, offset, start_date_id, end_date_id)
+
 
         if obj.__class__.__name__ == 'VenueSummary': 
             # retain only alpha chars
@@ -62,17 +64,26 @@ def process_summaries(obj, conn: Connection, date_int: int, date_string: str):
             prompt = summtools.summary_prompt(input_json, obj.table_name)
             output_table = aitools.process_completion(client, prompt, summtools.JSON_FORMAT)
 
-    
+            # print()
+            # print("VenueID: "+ str(summtools.get_unique_ids(input_tbl, 'VenueID')), "Count Reviews: " + str(len(input_tbl)))
+            # pd.set_option('display.max_colwidth', None)
+            # print(output_table['Pros'].to_string())
+            # print(output_table['Cons'].to_string())
+            # print()
+
+            # replace None etc with '-'
+        
+        
         # check valid summary length.
         summary = output_table['Summary'].to_list()
         summary_wordlen = len(summary[0].split(' '))
 
-        if try_count != 3:
+        if try_count != 3 and len(output_table.index) == 1:
             if summary_wordlen <= 50:
                 conn.execute(sa.text(aitools.drop_tbl_query('#temp')))
                 aitools.table_to_sqltbl(base_tbl=output_table, sql_tbl_name='#temp', conn=conn)
 
-                conn.execute(sa.text(obj.temp_insert(date_string, PK_ID, FK_ID)))
+                conn.execute(sa.text(obj.temp_insert(start_date_id, end_date_id, PK_ID, FK_ID)))
                 completed += 1
             else:
                 try_count += 1
@@ -85,28 +96,22 @@ def process_summaries(obj, conn: Connection, date_int: int, date_string: str):
 
         try_count = 1
 
-        flush = False if remaining <= 1 else True
-        aitools.print_result(completed, remaining, failed, flush)
+        aitools.print_result(1, completed, remaining, failed)
 
 
-def main():
+def main(rep_MMM_YY: str, num_months: int):
     with engine.begin() as conn:
-        dateid_tbl = pd.read_sql(sa.text(MMM_YYtoDate), conn)
-        date_string = str(dateid_tbl['ActualDate'][0])
-
-        dateid_intquery = f"SELECT DateID FROM Dates d where d.ActualDate = '{date_string}'"
-        dateid_tbl = pd.read_sql(sa.text(dateid_intquery), conn)
-        date_int = int(dateid_tbl['DateID'][0])
 
         aitools.create_temp(conn, '#SummaryVenue', 'Summary_Venue')
         aitools.create_temp(conn, '#SummaryOperator', 'Summary_Operator')
         aitools.create_temp(conn, '#SummaryRegion', 'Summary_Region')
+        aitools.create_temp(conn, '#DateRange', 'DateRange')
 
-        process_summaries(summtools.VenueSummary(), conn, date_int, date_string)
-        process_summaries(summtools.OperatorSummary(), conn, date_int, date_string)
-        process_summaries(summtools.RegionSummary(), conn, date_int, date_string)
+        process_summaries(summtools.VenueSummary(), conn, rep_MMM_YY, num_months)
+        process_summaries(summtools.OperatorSummary(), conn, rep_MMM_YY, num_months)
+        process_summaries(summtools.RegionSummary(), conn, rep_MMM_YY, num_months)
 
         conn.execute(sa.text(summtools.final_insert()))
 
 if __name__ == '__main__':
-    main()
+    main('FEB-25', 12)
--- tools/summarytools.py
+++ tools/summarytools.py
@@ -12,8 +12,8 @@ class VenueSummary():
     def get_count_remaining(
         self,
         conn: Connection,
-        date_int: int,
-        date_string: Optional[str] = None) -> int:
+        start_date_id: int,
+        end_date_id: int) -> int:
 
         query =  f"""
             SELECT count(distinct(r.VenueID))
@@ -24,17 +24,17 @@ class VenueSummary():
                 LEFT JOIN Review r1 on r1.VenueID = v.VenueID
                 WHERE r1.VenueID is not null
             ) x ON x.VenueID = r.VenueID
-            INNER JOIN Dates rev on rev.DateID = r.Review_DateID
-            LEFT JOIN #SummaryVenue sv on sv.VenueID = r.VenueID and sv.DateID = {date_int}
-            WHERE ReviewText IS NOT NULL AND 
-                sv.VenueSummaryID IS NULL AND
-                YEAR(rev.ActualDate) = YEAR('{date_string}') and MONTH(rev.ActualDate) = MONTH('{date_string}')
+            INNER JOIN #DateRange dr on dr.StartDateID = {start_date_id} AND dr.EndDateID = {end_date_id}
+            LEFT JOIN #SummaryVenue sv on sv.VenueID = r.VenueID and sv.DateRangeID = dr.DateRangeID
+            WHERE 
+                ReviewText IS NOT NULL AND 
+                sv.VenueSummaryID IS NULL
+                AND r.Review_DateID BETWEEN {start_date_id} and {end_date_id}
                 AND NOT EXISTS (
                     SELECT * FROM Summary_Venue s_v
                     WHERE x.VenueID = s_v.VenueID
-                    AND s_v.DateID = {date_int}
+                    AND s_v.DateRangeID = dr.DateRangeID
                 )
-
             """
         
         count = int(pd.read_sql(sa.text(query), conn).to_string(index=False).strip())
@@ -44,8 +44,8 @@ class VenueSummary():
         self,
         conn: Connection,
         offset: Optional[int],
-        date_int: int,
-        date_string: Optional[str] = None
+        start_date_id: int,
+        end_date_id: int
         ) -> pd.DataFrame:
 
         query =  f"""
@@ -53,16 +53,15 @@ class VenueSummary():
                 SELECT DISTINCT v.VenueID, ReviewID
                 FROM Venue v
                 LEFT JOIN Review r1 on r1.VenueID = v.VenueID
-                LEFT JOIN #SummaryVenue sv on sv.VenueID = v.VenueID and sv.DateID = {date_int}
-                INNER JOIN Dates rev on rev.DateID = Review_DateID
+                INNER JOIN #DateRange dr on dr.StartDateID = {start_date_id} AND dr.EndDateID = {end_date_id}
+                LEFT JOIN #SummaryVenue sv on sv.VenueID = r1.VenueID and sv.DateRangeID = dr.DateRangeID
                 WHERE r1.ReviewText IS NOT NULL
                     AND sv.VenueSummaryID IS NULL
-                    AND YEAR(rev.ActualDate) = YEAR('{date_string}')
-                    AND MONTH(rev.ActualDate) = MONTH('{date_string}')
+                    AND r1.Review_DateID BETWEEN {start_date_id} and {end_date_id}
                     AND NOT EXISTS (
                         SELECT * FROM Summary_Venue s_v
                         WHERE v.VenueID = s_v.VenueID
-                          AND s_v.DateID = {date_int}
+                          AND s_v.DateRangeID = dr.DateRangeID
                     )
             )
             SELECT r.VenueID,
@@ -83,19 +82,20 @@ class VenueSummary():
 
     def temp_insert(
         self,
-        date_string: str,
+        start_date_id: int,
+        end_date_id: int,
         venueid: int,
         id2: Optional[int] = None) -> str:
 
         query = f"""
-                INSERT INTO #SummaryVenue (VenueSummary, DateID, VenueID, RegionSummaryID, OperatorSummaryID)
-                SELECT t.Summary, d.DateID, {venueid}, 0, 0
+                INSERT INTO #SummaryVenue (VenueSummary, VenuePros, VenueCons, DateRangeID, VenueID, RegionSummaryID, OperatorSummaryID)
+                SELECT t.Summary, t.Pros, t.Cons, dr.DateRangeID, {venueid}, 0, 0
                 FROM #temp t
-                INNER JOIN Dates d on d.ActualDate = '{date_string}'
+                INNER JOIN #DateRange dr on dr.StartDateID = {start_date_id} and dr.EndDateID = {end_date_id}
                 WHERE NOT EXISTS (
                         SELECT * FROM #SummaryVenue
                         WHERE #SummaryVenue.VenueID = {venueid} AND
-                            #SummaryVenue.DateID = d.DateID
+                            #SummaryVenue.DateRangeID = dr.DateRangeID
                         )
                 """
         return query
@@ -110,18 +110,18 @@ class OperatorSummary():
     def get_count_remaining(
         self,
         conn: Connection,
-        date_int: int,
-        date_string: Optional[str] = None) -> int:
+        start_date_id: int,
+        end_date_id: int) -> int:
 
         query = f"""
             SELECT count(distinct(rem_op.OperatorID)) from (
-                    select VenueSummary, o.OperatorID, sv.DateID
+                    select VenueSummary, o.OperatorID, sv.DateRangeID
                     from #SummaryVenue sv
                     inner join Venue v on v.VenueID = sv.VenueID
                     inner join Brand b on b.BrandID = v.BrandID
                     inner join Operator o on o.OperatorID = b.OperatorID
                     LEFT JOIN #SummaryOperator so on so.OperatorID = o.OperatorID
-                                                 and so.DateID = {date_int}
+                                                 and so.DateRangeID = sv.DateRangeID
                     WHERE so.OperatorID is null
                 ) rem_op
             """
@@ -133,18 +133,18 @@ class OperatorSummary():
         self,
         conn: Connection,
         offset: int,
-        date_int: int,
-        date_string: Optional[str] = None
+        start_date_id: int,
+        end_date_id: int
         ) -> pd.DataFrame:
 
         query = f"""
             WITH cte as (
-                select VenueSummary, o.OperatorID, sv.DateID
+                select VenueSummary, o.OperatorID, sv.DateRangeID
                 from #SummaryVenue sv
                 inner join Venue v on v.VenueID = sv.VenueID
                 inner join Brand b on b.BrandID = v.BrandID
                 inner join Operator o on o.OperatorID = b.OperatorID
-                LEFT JOIN #SummaryOperator so on so.OperatorID = o.OperatorID and sv.DateID = {date_int}
+                LEFT JOIN #SummaryOperator so on so.OperatorID = o.OperatorID and so.DateRangeID = sv.DateRangeID
                 WHERE so.OperatorID IS NULL
             )
             select distinct * from cte
@@ -160,19 +160,20 @@ class OperatorSummary():
 
     def temp_insert(
         self,
-        date_string: str,
+        start_date_id: int,
+        end_date_id: int,
         operatorid: int,
         id2: Optional[int] = None) -> str:
 
         query = f"""
-                INSERT INTO #SummaryOperator (OperatorSummary, DateID, OperatorID)
-                SELECT t.Summary, d.DateID, {operatorid}
+                INSERT INTO #SummaryOperator (OperatorSummary, OperatorPros, OperatorCons, DateRangeID, OperatorID)
+                SELECT t.Summary, t.Pros, t.Cons, dr.DateRangeID, {operatorid}
                 FROM #temp t
-                INNER JOIN Dates d on d.ActualDate = '{date_string}'
+                INNER JOIN #DateRange dr on dr.StartDateID = {start_date_id} and dr.EndDateID = {end_date_id}
                 WHERE NOT EXISTS (
                         SELECT * FROM #SummaryOperator
                         WHERE #SummaryOperator.OperatorID = {operatorid} AND
-                            #SummaryOperator.DateID = d.DateID
+                            #SummaryOperator.DateRangeID = dr.DateRangeID
                         )
                 """
         return query
@@ -188,13 +189,14 @@ class RegionSummary():
     def get_count_remaining(
         self,
         conn: Connection,
-        date_int: int,
-        date_string: Optional[str] = None) -> int:
+        start_date_id: int,
+        end_date_id: int) -> int:
 
         query = f"""
             WITH cte AS (
             select distinct operatorid, regionid from (
-                    select v.VenueID, VenueSummary, o.OperatorID, sv.DateID, reg.RegionID from #SummaryVenue sv
+                    select v.VenueID, VenueSummary, o.OperatorID, sv.DateRangeID, reg.RegionID
+                    from #SummaryVenue sv
                     inner join Venue v on v.VenueID = sv.VenueID
                     inner join Brand b on b.BrandID = v.BrandID
                     inner join Operator o on o.OperatorID = b.OperatorID
@@ -202,7 +204,7 @@ class RegionSummary():
                     inner join Region reg on reg.RegionID = p.RegionID
                     LEFT JOIN #SummaryRegion sr on sr.RegionID = reg.RegionID
                          and sr.OperatorID = o.OperatorID 
-                         and sv.DateID = {date_int}
+                         and sr.DateRangeID = sv.DateRangeID
                     WHERE sr.RegionSummaryID IS NULL
                 ) reg_op
             )
@@ -217,13 +219,13 @@ class RegionSummary():
         self,
         conn: Connection,
         offset: int,
-        date_int: int,
-        date_string: Optional[str] = None,
+        start_date_id: int,
+        end_date_id: int
         ) -> pd.DataFrame:
 
         query = f"""
             WITH cte as (
-                select v.VenueID, VenueSummary, o.OperatorID, sv.DateID, reg.RegionID from #SummaryVenue sv
+                select v.VenueID, VenueSummary, o.OperatorID, sv.DateRangeID, reg.RegionID from #SummaryVenue sv
                 inner join Venue v on v.VenueID = sv.VenueID
                 inner join Brand b on b.BrandID = v.BrandID
                 inner join Operator o on o.OperatorID = b.OperatorID
@@ -231,10 +233,10 @@ class RegionSummary():
                 inner join Region reg on reg.RegionID = p.RegionID
                 LEFT JOIN #SummaryRegion sr on sr.RegionID = reg.RegionID
                                           and sr.OperatorID = o.OperatorID
-                                          and sv.DateID = {date_int}
+                                          and sr.DateRangeID = sv.DateRangeID
                 WHERE sr.RegionSummaryID IS NULL
             )
-            select VenueID, VenueSummary, x.OperatorID, DateID, x.RegionID from cte
+            select VenueID, VenueSummary, x.OperatorID, DateRangeID, x.RegionID from cte
             inner join (
                 select distinct operatorid, regionid
                 from cte
@@ -251,20 +253,21 @@ class RegionSummary():
 
     def temp_insert(
         self,
-        date_string: str,
+        start_date_id: int,
+        end_date_id: int,
         regionid: int,
         operatorid: int) -> str:
 
         query = f"""
-            INSERT INTO #SummaryRegion (RegionSummary, DateID, RegionID, OperatorID)
-            SELECT t.Summary, d.DateID, {regionid}, {operatorid}
+            INSERT INTO #SummaryRegion (RegionSummary, RegionPros, RegionCons, DateRangeID, RegionID, OperatorID)
+            SELECT t.Summary, t.Pros, t.Cons, dr.DateRangeID, {regionid}, {operatorid}
             FROM #temp t
-            INNER JOIN Dates d on d.ActualDate = '{date_string}'
+            INNER JOIN #DateRange dr on dr.StartDateID = {start_date_id} and dr.EndDateID = {end_date_id}
             WHERE NOT EXISTS (
                     SELECT * FROM #SummaryRegion
                     WHERE #SummaryRegion.OperatorID = {operatorid} AND
                         #SummaryRegion.RegionID = {regionid} AND
-                        #SummaryRegion.DateID = d.DateID
+                        #SummaryRegion.DateRangeID = dr.DateRangeID
                     )
             """
         return query
@@ -272,39 +275,80 @@ class RegionSummary():
 
 def summary_prompt(json, category: str) -> str:
     prompt = f"""
-        The following JSON contains restaurant reviews (ReviewText) for a {category}.
+
+        There are 3 categories: Venue, Region, and Operator.
+        The following JSON contains restaurant reviews (ReviewText) for the category: {category}.
         Each review is a separate entry.
-        Write a concise, up to 50 word summary of the reviews for the {category}. Only refer to the opinions
-        and sentiments in the reviews, and disregard the {category} name.
-        Don't mention anything regarding closure or opening of a venue if that is the category.
-        Don't mention names. Be as objective and holistic as possible.
+        
+        A) Write a concise, up to 50 word summary of the reviews for that category. 
+           Requirements:
+           1) Only refer to the opinions and sentiments in the reviews.
+           2) Don't mention anything regarding closure or opening of a venue if that is the category.
+           3) Don't mention the names of people. Be as objective and holistic as possible.
+           4) Do not mention a category that is not the category '{category}' in the summary.
+           5) If the category is not 'Venue', then the summary should refer to the venues WITHIN the category.
+
+        B) Then, provide a short-form list of up to 3 pros based on the same reviews.
+            Requirements:
+            1) After each item, list the % of reviews that reference that pro to the nearest 10%.
+            only count explicit mentions towards the percentage (e.g., "staff were great" counts, but vague compliments like "it was great" do not unless directly tied to a category).
+            Count each pro only once per review, even if mentioned multiple times in the same review.
+            2) Each item should follow the structure 'adjective + noun', e.g., great staff (20%) or nice environment (60%).
+            3) Group related terms/adjectives under one category where appropriate. For example:
+                If "great staff" is mentioned in one review and "friendly workers" in another, combine them under 'great staff'.
+            4) Ensure groupings are logical, objective, and not overly broad.
+            5) If there are no identifiable pros, return only '-'.
+            6) There should be no more than 1 row in the output. If there is, retry.
+
+        C) Repeat the process for up to 3 cons, using the same structure and approach.
 
         Here are the reviews: \n\n{json}
         """
     return prompt
 
 
+def date_range_insert(start_date_id, end_date_id, months):
+    query = f"""
+            INSERT INTO #DateRange (StartDateID, EndDateID, Months)
+            SELECT {start_date_id}, {end_date_id}, {months}
+            WHERE NOT EXISTS (SELECT * FROM #DateRange dr
+                              WHERE dr.StartDateID = {start_date_id} AND
+                                    dr.EndDateID = {end_date_id} AND
+                                    dr.Months = {months}
+                            )
+            """
+    return query
+
+
 def final_insert() -> str:
     query = """
-            INSERT INTO Summary_Operator (OperatorSummary, DateID, OperatorID)
-            SELECT OperatorSummary, DateID, OperatorID
+            INSERT INTO DateRange (StartDateID, EndDateID, Months)
+            SELECT StartDateID, EndDateID, Months FROM #DateRange d_r     
+            WHERE NOT EXISTS (SELECT * FROM DateRange dr
+                            WHERE d_r.StartDateID = dr.StartDateID AND 
+                                  d_r.EndDateID = dr.EndDateID AND
+                                  d_r.Months = dr.Months)      
+
+
+            INSERT INTO Summary_Operator (OperatorSummary, OperatorPros, OperatorCons, DateRangeID, OperatorID)
+            SELECT OperatorSummary, OperatorPros, OperatorCons, DateRangeID, OperatorID
             FROM #SummaryOperator so
             WHERE NOT EXISTS (SELECT * FROM Summary_Operator s_o
-                            WHERE s_o.DateID = so.DateID AND
+                            WHERE s_o.DateRangeID = so.DateRangeID AND
                                   s_o.OperatorID = so.OperatorID)                         
 
                                     
-            INSERT INTO Summary_Region (RegionSummary, DateID, RegionID, OperatorID)
-            SELECT RegionSummary, DateID, RegionID, OperatorID
+            INSERT INTO Summary_Region (RegionSummary, RegionPros, RegionCons, DateRangeID, RegionID, OperatorID)
+            SELECT RegionSummary, RegionPros, RegionCons, DateRangeID, RegionID, OperatorID
             FROM #SummaryRegion sr                    
             WHERE NOT EXISTS (SELECT * FROM Summary_Region s_r
-                            WHERE s_r.DateID = sr.DateID AND
+                            WHERE s_r.DateRangeID = sr.DateRangeID AND
                                   s_r.OperatorID = sr.OperatorID AND
                                   s_r.RegionID = sr.RegionID)                         
 
                                             
-            INSERT INTO Summary_Venue (VenueSummary, DateID, VenueID, RegionSummaryID, OperatorSummaryID) 
-            SELECT sv.VenueSummary, sv.DateID, sv.VenueID, sr.RegionSummaryID, so.OperatorSummaryID
+            INSERT INTO Summary_Venue (VenueSummary, VenuePros, VenueCons, DateRangeID, VenueID, RegionSummaryID, OperatorSummaryID) 
+            SELECT sv.VenueSummary, sv.VenuePros, sv.VenueCons, sv.DateRangeID, sv.VenueID, sr.RegionSummaryID, so.OperatorSummaryID
             FROM #SummaryVenue sv
             INNER JOIN Venue v ON v.VenueID = sv.VenueID
             INNER JOIN Brand b ON b.BrandID = v.BrandID
@@ -313,12 +357,13 @@ def final_insert() -> str:
             INNER JOIN Region r ON r.RegionID = p.RegionID
             INNER JOIN #SummaryRegion sr ON sr.RegionID = r.RegionID AND
                                             sr.OperatorID = o.OperatorID AND
-                                            sr.DateID = sv.DateID
+                                            sr.DateRangeID = sv.DateRangeID
             INNER JOIN #SummaryOperator so ON so.OperatorID = o.OperatorID AND
-                                            so.DateID = sv.DateID
+                                            so.DateRangeID = sv.DateRangeID
             WHERE NOT EXISTS (SELECT * FROM Summary_Venue s_v
-                            WHERE s_v.DateID = sv.DateID AND
-                                  s_v.VenueID = sv.VenueID)                                            
+                            WHERE s_v.DateRangeID = sv.DateRangeID AND
+                                  s_v.VenueID = sv.VenueID)          
+                  
             """
     return query
 
@@ -346,10 +391,18 @@ JSON_FORMAT = {
                                 "properties": {
                                     "Summary": {
                                         "type": "string"
+                                    },
+                                    "Pros": {
+                                        "type": "string"
+                                    },
+                                    "Cons": {
+                                        "type": "string"
                                     }
                                 },
                                 "required": [
-                                    "Summary"
+                                    "Summary",
+                                    "Pros",
+                                    "Cons"
                                     ],
                                 "additionalProperties": False
                             
